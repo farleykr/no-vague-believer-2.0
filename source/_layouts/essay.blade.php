cf5367778e83539fd7e24dfed450aa07eee0ad29
Farley Reynolds <farleykreynolds@gmail.com>
2021-01-13 09:35:11 -0500

    create essay layout

---
+++ source/_layouts/essay.blade.php
@@ -0,0 +1,29 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="p-2 mb-24">
+    <section id="newest" class="mb-24">
+        <div class="text-center my-20">
+            <p class="uppercase">Newest Essay</p>
+            <h1 class="text-4xl leading-9 my-2">{{ $page->title }}</h1>
+            <date class="block mb-3">{{ date('m/d/Y', $page->date) }}</date>
+        </div>
+
+        <div class="essay-body">
+            <aside class="mb-2 text-gray-500">
+                @if ($page->verse)
+                    <p class="italic">{{ $page->verse }}</p>
+                @endif
+                @if ($page->citation)
+                    <p>{{ $page->citation }}</p>
+                    <p>&mdash;</p>
+                @endif
+            </aside>
+            @yield('essay_body')
+            <p class="text-right italic">&mdash; {{ $page->author }} </p>
+        </div>
+    </section>
+    
+    @include('_partials.archive')
+</div>
+@endsection
