068d42a1ecdd5e09e9a7ffcb20b8b56bf16ae45a
Farley Reynolds <farleykreynolds@gmail.com>
2021-01-13 09:59:14 -0500

    design tweaks to archive

--- source/_partials/archive.blade.php
+++ source/_partials/archive.blade.php
@@ -1,13 +1,21 @@
 <section id="archive">
-    <p class="pb-4 uppercase text-center border-b border-gray-400">All Essays</p>
+    <p class="pb-6 uppercase text-center border-b border-gray-400">All Essays</p>
     <ul>
         @foreach($essays as $essay)
-            <li class="py-4 border-b border-gray-400">
+            <li class="py-6 border-b border-gray-400">
+                <div class="flex justify-between">
                 <h2>
                     <a href="{{ $essay->getUrl() }}">{{ $essay->title }}</a>
                 </h2>
                 <date class="block mb-2">{{ date('m/d/Y', $essay->date) }}</date>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quibusdam officiis numquam, recusandae at voluptatum placeat. Consequuntur facilis libero minus!</p>
+                </div>
+                
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt quibusdam officiis numquam, recusandae at voluptatum placeat. Consequuntur facilis libero minus!
+                    
+                </p>
+                <a class="block italic text-right" href="{{ $essay->getUrl() }}">read &rarr;</a>
+                
             </li>
         @endforeach
     </ul>
