9386b57262db9269462f8a4747eef3da1f30254c
Farley Reynolds <farleykreynolds@gmail.com>
2021-01-13 09:49:19 -0500

    design tweaks to header and footer

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-gray-200">
-    <div class="max-w-xl mx-auto p-2">
+    <div class="max-w-xl mx-auto px-2 py-6">
         <p class="border-b border-gray-900 pb-2 mb-2">
             "I am no vague believer. I see from the standpoint of Christian orthodoxy. This means that for me the meaning of life is centered in our Redemption by Christ and that what I see in the world I see its relation to that."
             <br>
--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,6 +1,8 @@
 <header class="sticky top-0 bg-gray-900 text-gray-100">
     <div class="max-w-xl mx-auto flex flex-row justify-between align-center p-2">
-        <h2 class="text-xl uppercase">No Vague Believer</h2>
+        <h2 class="text-xl uppercase underline">
+            <a href="/">No Vague Believer</a>
+        </h2>
         <button class="px-2 border border-gray-100">subscribe</button>
     </div>
 </header>
