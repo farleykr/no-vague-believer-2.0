31726601fd8037fd4b1bab5a2ec9048558ea7036
Farley Reynolds <farleykreynolds@gmail.com>
2021-01-15 07:33:53 -0500

    design adjustments

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="text-gray-800">
     <div class="max-w-xl mx-auto px-3 py-6">
-        <p class="border-b border-t border-gray-800 py-2 mb-2 text-center">
+        <p class="border-b border-gray-800 py-2 mb-2">
             <span class="italic">
             "I am <span class="harbour-md text-gray-800">no vague believer</span>. I see from the standpoint of Christian orthodoxy. This means that for me the meaning of life is centered in our Redemption by Christ and that what I see in the world I see its relation to that."
             </span>
--- source/_partials/subscribe.blade.php
+++ source/_partials/subscribe.blade.php
@@ -1,11 +1,11 @@
-<section id="subscribe" class="bg-gray-50 text-gray-800">
+<section id="subscribe" class="bg-gray-100 text-gray-800">
     <div class="max-w-xl mx-auto px-3 py-6">
-        <h3 class="text-2xl pb-6 uppercase text-center border-b border-gray-100 font-sans font-bold">Subscribe</h3>
+        <h3 class="text-2xl pb-6 uppercase text-center border-b border-gray-900 font-sans font-bold">Subscribe</h3>
         <div class="pt-6 text-center">
             <p class="mb-2 italic">Sign up to receive new essays in your email inbox as they are published.</p>
             <form class="mb-2" action="https://tinyletter.com/novaguebeliever" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/novaguebeliever', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
                 <label class="block" for="tlemail">Email address:</label>
-                <input class="block w-56 m-2 border border-gray-200 mx-auto text-center" type="text" name="email" id="tlemail"/>
+                <input class="block w-56 m-2 border border-gray-200 mx-auto font-mono text-center" type="text" name="email" id="tlemail"/>
                 <input type="hidden" value="1" name="embed"/>
                 <button class="block mx-auto px-3 border bg-gray-800 shadow-sm font-sans text-white uppercase" type="submit" value="subscribe"/>Subscribe
                 </button>
